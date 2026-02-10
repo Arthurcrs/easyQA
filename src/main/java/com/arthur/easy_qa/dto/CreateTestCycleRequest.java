fd85fd483516ed2535cc97c7c62a9d9c864212fe
Arthur Carlos da Rocha Silva <acrs@cesar.org.br>
2026-02-09 22:09:12 -0300

    Fixed TestCycleController tests

--- src/main/java/com/arthur/easy_qa/dto/CreateTestCycleRequest.java
+++ src/main/java/com/arthur/easy_qa/dto/CreateTestCycleRequest.java
@@ -5,6 +5,24 @@ import jakarta.validation.constraints.NotBlank;
 public class CreateTestCycleRequest {
 
     @NotBlank
-    String name;
+    private String name;
+    private String version;
+    private String type;
+    private String environment;
 
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getVersion() {
+        return version;
+    }
 }
