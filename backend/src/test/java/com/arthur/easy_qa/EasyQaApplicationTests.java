9f3e9811b80cfd28d44c11924e2b8d4d0db1debb
Arthur Carlos da Rocha Silva <acrs@cesar.org.br>
2026-03-30 15:56:01 -0300

    Removed the empty line at the end of each class

--- backend/src/test/java/com/arthur/easy_qa/EasyQaApplicationTests.java
+++ backend/src/test/java/com/arthur/easy_qa/EasyQaApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EasyQaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
